80b88e3cf4adc2946ddd09f5a84adb2bb1dd711d
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2018-11-02 18:15:53 +0000

    Cron Jobs

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const project = require('pillars');
 const GDB = require("@goblindb/goblindb");
+const Scheduled = require("scheduled");
 const config = require("./config");
 const apiManagement = require('./api');
-
+const twitter_task = require("./scheduled_tasks/twitter");
+const meetup_task = require("./scheduled_tasks/meetup");
+const slack_task = require("./scheduled_tasks/slack");
+const github_task = require("./scheduled_tasks/github");
 
 // Wake up GoblinDB
 const goblinDB = GDB({
@@ -15,6 +19,7 @@ const goblinDB = GDB({
         }).start();
 });
 
+// Routes definition
 const apiRoutes = new Route({
     id: 'apiRoutes',
     path: 'api/v1/*:path',
@@ -48,3 +53,33 @@ const staticFiles = new Route({
 project.routes.add(apiRoutes);
 project.routes.add(rootRoutes);
 project.routes.add(staticFiles);
+
+
+// Cron Jobs definition
+const dailyTasks = new Scheduled({
+    id: "dailyTasks",
+    pattern: "00 9 * * * *", // 09:00 AM every day
+    task: () => {
+        slack_task.slackInfo(goblinDB);
+        twitter_task.twitterInfo(goblinDB);
+        meetup_task.meetupInfo(goblinDB);
+    }
+});
+
+const hourlyTask = new Scheduled({
+    id: "hourlyTask",
+    pattern: "0 * * * * *", // Every Hour
+    task: () => {
+        github_task.allData(goblinDB);
+    }
+});
+
+// Start Jobs
+dailyTasks.start();
+hourlyTask.start();
+
+// Autostart Jobs
+/*
+dailyTasks.launch();
+hourlyTask.launch();
+*/
