81830d521e0e70f5992a7946daf9b1571dc18852
Teba Gomez <gomez.teba@gmail.com>
2019-01-07 16:07:26 +0000

    Gestion de errores y documentacion. #14

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,21 @@
+// Variables
 const loadingSlctr = document.getElementById("loading-status");
+const errorSlctr = document.getElementById("error-data");
 
+
+// Functions
+
+/** 
+ * Ajax request handler
+ * @param {string} url - the URL which we're addressing to for the AJAX request
+ * @param {requestCallback} cb - The callback that handles the response.
+ */
+ 
+ /**
+ * @callback requestCallback
+ * @param {boolean} err - has an error true/false
+ * @param {object|boolean} data - The information/response from the server
+ */
 function ajaxRequest(url, cb) {
     const request = new XMLHttpRequest();
 
@@ -7,7 +23,7 @@ function ajaxRequest(url, cb) {
 
         if (request.readyState === 4 && request.status === 200) {
             cb(false, JSON.parse(request.responseText));
-        } else if (request.readyState === 4 && request.status === 404) {
+        } else if (request.readyState === 4 && request.status !== 200) {
             cb(true);
         }
     };
@@ -16,12 +32,22 @@ function ajaxRequest(url, cb) {
     request.send();
 }
 
+/** 
+ * Hides loading page image
+ */
 function hideLoading () {
     loadingSlctr.style.display = "none";
 }
 
+/** 
+ * Displays error message
+ */
+function errorShow(){
+    errorSlctr.style.display = "inherit";
+}
+
 
 ajaxRequest("api/v1", function (err, data) {
+    err ? errorShow() : console.log("Data:", data);
     hideLoading();
-    console.log("Data:", data);
 });
